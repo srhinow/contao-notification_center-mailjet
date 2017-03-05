fe4117fdd10d4323eda5e05eb6da7c9982583f65
Wusch <florian.otto@mindbird.de>
2017-03-05 17:33:37 +0100

    Initial

---
+++ contao/dca/tl_nc_gateway.php
@@ -0,0 +1,23 @@
+<?php
+$GLOBALS['TL_DCA']['tl_nc_gateway']['fields']['mailjet_apikey_private'] = array(
+    'label' => &$GLOBALS['TL_LANG']['tl_settings']['mailjet_apikey_private'],
+    'exclude' => true,
+    'inputType' => 'text',
+    'eval' => array(
+        'mandatory' => true,
+        'maxlength' => 255,
+        'tl_class' => 'w50'
+    )
+);
+$GLOBALS['TL_DCA']['tl_nc_gateway']['fields']['mailjet_apikey_public'] = array(
+    'label' => &$GLOBALS['TL_LANG']['tl_settings']['mailjet_apikey_public'],
+    'exclude' => true,
+    'inputType' => 'text',
+    'eval' => array(
+        'mandatory' => true,
+        'maxlength' => 255,
+        'tl_class' => 'w50'
+    )
+);
+
+$GLOBALS['TL_DCA']['tl_nc_gateway']['mailjet'] = '{title_legend},title,type;{gateway_legend},mailjet_apikey_public,mailjet_apikey_private';
--- contao/language/de/tl_settings.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$GLOBALS['TL_LANG']['tl_settings']['be_notification_center_mailjet_legend'] = 'Mailjet';
-$GLOBALS['TL_LANG']['tl_settings']['be_notification_center_mailjet_apikey_private'] = array('Geheimer Key', '');
-$GLOBALS['TL_LANG']['tl_settings']['be_notification_center_mailjet_apikey_public'] = array('API Key', '');
