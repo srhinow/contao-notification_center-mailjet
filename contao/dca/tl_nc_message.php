936abad92b6e1e9f85e7e313da060988e9664b86
Wusch <florian.otto@mindbird.de>
2017-03-05 18:50:56 +0100

    Initial

--- contao/dca/tl_nc_message.php
+++ contao/dca/tl_nc_message.php
@@ -1,7 +1,8 @@
 <?php
 
 $GLOBALS['TL_DCA']['tl_nc_message']['fields']['mailjet_template'] = array(
-    'load_callback' => array('mindbird\NotificationCenter\Table\Message', 'listMailjetTemplates')
+    'load_callback' => array(
+        array('mindbird\NotificationCenter\Table\Message', 'listMailjetTemplates'))
 );
 
 $GLOBALS['TL_DCA']['tl_nc_message']['palettes']['mailjet'] = '{title_legend},title,gateway;{mailjet_legend},mailjet_template;{publish_legend},published';
