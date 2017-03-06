6df3c0fe47c99b75077c00f6f0e7b4411cdb74ab
Wusch <florian.otto@mindbird.de>
2017-03-07 00:19:23 +0100

    Initial

--- contao/languages/de/tl_nc_message.php
+++ contao/languages/de/tl_nc_message.php
@@ -4,3 +4,9 @@ $GLOBALS['TL_LANG']['tl_nc_message']['mailjet_legend'] = 'Mailjet Transaktionsma
 
 $GLOBALS['TL_LANG']['tl_nc_message']['mailjet_template'] = array('Template', '');
 $GLOBALS['TL_LANG']['tl_nc_message']['mailjet_recipients'] = array('Empfänger', '');
+$GLOBALS['TL_LANG']['tl_nc_message']['mailjet_sender_address']['0']     = 'Absender-E-Mail';
+$GLOBALS['TL_LANG']['tl_nc_message']['mailjet_sender_address']['1']     = 'Bitte geben Sie die Absender-Adresse ein.';
+$GLOBALS['TL_LANG']['tl_nc_message']['mailjet_sender_name']['0']        = 'Absendername';
+$GLOBALS['TL_LANG']['tl_nc_message']['mailjet_sender_name']['1']        = 'Bitte geben Sie den Absender ein.';
+$GLOBALS['TL_LANG']['tl_nc_message']['mailjet_subject']['0']            = 'Subject';
+$GLOBALS['TL_LANG']['tl_nc_message']['mailjet_subject']['1']            = 'Bite den Betreff für das Email eingeben.';
