894f871a8faf05d114755ba36557ef6be25cd8d2
Wusch <florian.otto@mindbird.de>
2017-03-06 08:08:59 +0100

    Initial

--- contao/dca/tl_nc_gateway.php
+++ contao/dca/tl_nc_gateway.php
@@ -6,10 +6,10 @@ $GLOBALS['TL_DCA']['tl_nc_gateway']['fields']['mailjet_apikey_private'] = array(
     'inputType' => 'text',
     'eval' => array(
         'mandatory' => true,
-        'maxlength' => 255,
+        'maxlength' => 64,
         'tl_class' => 'w50'
     ),
-    'sql' => "varchar(255) NOT NULL default ''"
+    'sql' => "varchar(64) NOT NULL default ''"
 );
 
 $GLOBALS['TL_DCA']['tl_nc_gateway']['fields']['mailjet_apikey_public'] = array(
@@ -18,10 +18,10 @@ $GLOBALS['TL_DCA']['tl_nc_gateway']['fields']['mailjet_apikey_public'] = array(
     'inputType' => 'text',
     'eval' => array(
         'mandatory' => true,
-        'maxlength' => 255,
+        'maxlength' => 64,
         'tl_class' => 'w50'
     ),
-    'sql' => "varchar(255) NOT NULL default ''"
+    'sql' => "varchar(64) NOT NULL default ''"
 );
 
 $GLOBALS['TL_DCA']['tl_nc_gateway']['palettes']['mailjet'] = '{title_legend},title,type;{gateway_legend},mailjet_apikey_public,mailjet_apikey_private';
