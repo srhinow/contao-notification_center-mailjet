7a8eb079e7426c9bb8f0144c11db42c9124f5104
Wusch <florian.otto@mindbird.de>
2019-03-01 11:16:00 +0100

    GNFSGHZBRWE

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,5 +1,3 @@
 <?php
 
-var_dump($GLOBALS['NOTIFICATION_CENTER']['GATEWAY']);
 $GLOBALS['NOTIFICATION_CENTER']['GATEWAY']['mailjet'] = Mindbird\Contao\MailjetNotification\Gateway\Mailjet::class;
-var_dump($GLOBALS['NOTIFICATION_CENTER']['GATEWAY']);
