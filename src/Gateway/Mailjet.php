607be50030927fbf15ece916d425b21670f54a13
Florian Otto <florian.otto@mindbird.de>
2019-04-23 11:40:58 +0200

    Send all token to mailjet Add attachments

--- src/Gateway/Mailjet.php
+++ src/Gateway/Mailjet.php
@@ -10,44 +10,93 @@
 
 namespace Mindbird\Contao\MailjetNotification\Gateway;
 
+use Contao\CoreBundle\Monolog\ContaoContext;
+use Contao\System;
 use Mailjet\Client;
 use Mailjet\Resources;
+use Mindbird\Contao\MailjetNotification\MessageDraft\MailjetMessageDraft;
+use NotificationCenter\Gateway\Base;
 use NotificationCenter\Gateway\GatewayInterface;
+use NotificationCenter\MessageDraft\MessageDraftFactoryInterface;
+use NotificationCenter\MessageDraft\MessageDraftInterface;
 use NotificationCenter\Model\Gateway;
+use NotificationCenter\Model\Language;
 use NotificationCenter\Model\Message;
 use NotificationCenter\Util\StringUtil;
+use Psr\Log\LogLevel;
 
-class Mailjet implements GatewayInterface
+class Mailjet extends Base implements GatewayInterface, MessageDraftFactoryInterface
 {
     public function send(Message $objMessage, array $arrTokens, $strLanguage = '')
     {
         $gateway = Gateway::findBy('id', $objMessage->gateway);
-        $vars = [];
-        foreach ($arrTokens as $key => $value) {
-            if (preg_match_all('/form_/', $key)) {
-                $vars[$key] = $value;
-            }
-        }
+        $draft = $this->createDraft($objMessage, $arrTokens, $strLanguage);
+        $this->sendDraft($draft, $gateway, $objMessage, $token);
+    }
 
+    /**
+     * Creates a MessageDraft
+     * @param Message
+     * @param array
+     * @param string
+     * @return  MessageDraftInterface|null (if no draft could be found)
+     */
+    public function createDraft(Message $objMessage, array $arrTokens, $strLanguage = '')
+    {
+        if ($strLanguage == '') {
+            $strLanguage = $GLOBALS['TL_LANGUAGE'];
+        }
+        if (($objLanguage = Language::findByMessageAndLanguageOrFallback($objMessage, $strLanguage)) === null) {
+            \System::log(sprintf('Could not find matching language or fallback for message ID "%s" and language "%s".', $objMessage->id, $strLanguage), __METHOD__, TL_ERROR);
+            return null;
+        }
+        return new MailjetMessageDraft($objMessage, $objLanguage, $arrTokens);
+    }
+
+    /**
+     * @param MailjetMessageDraft $objDraft
+     * @return bool
+     * @throws \Exception
+     */
+    public function sendDraft(MailjetMessageDraft $objDraft, Gateway $gateway, Message $message, $token)
+    {
         $mailjet = new Client($gateway->mailjet_apikey_public, $gateway->mailjet_apikey_private);
         $recipients = [];
-        foreach (StringUtil::compileRecipients($objMessage->mailjet_recipients, $arrTokens) as $recipient) {
+        foreach (StringUtil::compileRecipients($message->mailjet_recipients, $token) as $recipient) {
             $recipients[] = ['Email' => $recipient];
         }
 
+        $attachements = [];
+
+        // Add file attachments
+        $arrAttachments = $objDraft->getAttachments();
+        if (!empty($arrAttachments)) {
+            $attachements = array_merge($attachements, $arrAttachments);
+        }
+        // Add string attachments
+        $arrAttachments = $objDraft->getStringAttachments();
+        if (!empty($arrAttachments)) {
+            $attachements = array_merge($attachements, $arrAttachments);
+        }
+
         $body = [
-            'FromEmail' => $objMessage->mailjet_sender_address,
-            'FromName' => $objMessage->mailjet_sender_name,
-            'Subject' => $objMessage->mailjet_subject,
+            'FromEmail' => $message->mailjet_sender_address,
+            'FromName' => $message->mailjet_sender_name,
+            'Subject' => $message->mailjet_subject,
             'MJ-TemplateErrorReporting' => $GLOBALS['TL_CONFIG']['adminEmail'],
-            'MJ-TemplateID' => $objMessage->mailjet_template,
+            'MJ-TemplateID' => $message->mailjet_template,
             'MJ-TemplateLanguage' => true,
             'Recipients' => $recipients,
-            'Vars' => $vars,
+            'Vars' => $token,
+            'Attachments' => $attachements
         ];
         $response = $mailjet->post(Resources::$Email, ['body' => $body]);
         if (!$response->success()) {
-            dump($response);
+            $logger = System::getContainer()->get('monolog.logger.contao');
+            $logger->log(LogLevel::ERROR, $response, array('contao' => new ContaoContext(__FUNCTION__, __CLASS__)));
+            return false;
         }
+
+        return true;
     }
 }
---
+++ src/MessageDraft/MailjetMessageDraft.php
@@ -0,0 +1,147 @@
+<?php
+
+
+namespace Mindbird\Contao\MailjetNotification\MessageDraft;
+
+
+use Contao\File;
+use NotificationCenter\MessageDraft\MessageDraftInterface;
+use NotificationCenter\Model\Language;
+use NotificationCenter\Model\Message;
+use NotificationCenter\Util\StringUtil;
+
+class MailjetMessageDraft implements MessageDraftInterface
+{
+    /**
+     * Message
+     * @var Message
+     */
+    protected $objMessage = null;
+    /**
+     * Language
+     * @var Language
+     */
+    protected $objLanguage = null;
+    /**
+     * Tokens
+     * @var array
+     */
+    protected $arrTokens = array();
+    /**
+     * File path attachments
+     * @var array
+     */
+    protected $attachments = null;
+
+    /**
+     * String attachments
+     * @var array
+     */
+    protected $stringAttachments = null;
+
+    /**
+     * Construct the object
+     * @param Message  $objMessage
+     * @param Language $objLanguage
+     * @param          $arrTokens
+     */
+    public function __construct(Message $objMessage, Language $objLanguage, $arrTokens)
+    {
+        $this->arrTokens   = $arrTokens;
+        $this->objLanguage = $objLanguage;
+        $this->objMessage  = $objMessage;
+    }
+
+    /**
+     * Returns the paths to attachments as an array
+     * @return  array
+     */
+    public function getAttachments()
+    {
+        if ($this->attachments === null) {
+            // Token attachments
+            $this->attachments = StringUtil::getTokenAttachments($this->objLanguage->attachment_tokens, $this->arrTokens);
+
+            // Add static attachments
+            $arrStaticAttachments = deserialize($this->objLanguage->attachments, true);
+            if (!empty($arrStaticAttachments)) {
+                $objFiles = \FilesModel::findMultipleByUuids($arrStaticAttachments);
+                if ($objFiles !== null) {
+                    while ($objFiles->next()) {
+                        $this->attachments[] = [
+                            'Base64Content' => base64_encode(file_get_contents(TL_ROOT . '/' . $objFiles->path)),
+                            'Filename' => $objFiles->filename,
+                            'ContentType' => $objFiles->mime
+                        ];
+                    }
+                }
+            }
+        }
+        return $this->attachments;
+    }
+
+
+    /**
+     * Returns the contents of attachments as an array (the key being the desired file name).
+     * @return  array
+     * @throws \Exception
+     */
+    public function getStringAttachments()
+    {
+        if ($this->stringAttachments === null) {
+            // Add attachment templates
+            $arrTemplateAttachments = deserialize($this->objLanguage->attachment_templates, true);
+            if (!empty($arrTemplateAttachments)) {
+                $objFiles = \FilesModel::findMultipleByUuids($arrTemplateAttachments);
+                if ($objFiles !== null) {
+                    while ($objFiles->next()) {
+                        $file = new File($objFiles->path, true);
+                        if (!$file->exists()) {
+                            continue;
+                        }
+                        $this->stringAttachments[$objFiles->name] = [
+                            'Base64Content' => base64_encode(\Haste\Util\StringUtil::recursiveReplaceTokensAndTags($file->getContent(), $this->arrTokens)),
+                            'Filename' => $file->filename,
+                            'ContentType' => $file->mime
+                        ];
+                    }
+                }
+            }
+        }
+        return $this->stringAttachments;
+    }
+
+    /**
+     * Set the attachments
+     *
+     * @param array $attachments
+     */
+    public function setAttachments(array $attachments)
+    {
+        $this->attachments = $attachments;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTokens()
+    {
+        return $this->arrTokens;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getMessage()
+    {
+        return $this->objMessage;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getLanguage()
+    {
+        return $this->objLanguage->language;
+    }
+}
